9d0b13022769f51b518963c28a28b71a983e0bd7
Cindy SCHAAL <schaal.cindy@gmail.com>
2026-03-27 15:13:44 +0100

    intialisation du projet

---
+++ src/main/java/com/project/gestion_notes/web/controller/NoteController.java
@@ -0,0 +1,4 @@
+package com.project.gestion_notes.web.controller;
+
+public class NoteController {
+}
---
+++ src/test/java/com/project/gestion_notes/GestionNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gestion_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
