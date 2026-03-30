128260cb73094fc0084eb6c1abe600987fef31ef
Cindy SCHAAL <schaal.cindy@gmail.com>
2026-03-30 15:17:43 +0200

    Mise en place de la BDD POSTGRES sur docker

--- src/main/java/com/project/gestion_notes/web/controller/NoteController.java
+++ src/main/java/com/project/gestion_notes/web/controller/NoteController.java
@@ -1,4 +1,15 @@
 package com.project.gestion_notes.web.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NoteController {
+
+    @GetMapping("/notes")
+    public String listeNotes() {
+        return "Liste des notes";
+    }
+
 }
