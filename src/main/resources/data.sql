65c131c61505536ea2ffac6e0f35f2b996733362
Cindy SCHAAL <schaal.cindy@gmail.com>
2026-04-02 11:37:21 +0200

    Changement dans la bdd pour la table note

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-/*INSERT INTO notes (note) VALUES (12);
-INSERT INTO notes (note) VALUES (18);
-INSERT INTO notes (note) VALUES (20);
+INSERT INTO notes (note, id_eleve, id_matiere) VALUES (12,1,2);
+INSERT INTO notes (note, id_eleve, id_matiere) VALUES (18, 2, 4);
+INSERT INTO notes (note, id_eleve, id_matiere) VALUES (20, 4, 2);
 
 INSERT INTO eleves (nom, prenom) VALUES ('Dupont', 'Jean');
 INSERT INTO eleves (nom, prenom) VALUES ('Martin', 'Sophie');
@@ -11,4 +11,4 @@ INSERT INTO eleves (nom, prenom) VALUES ('Bernard', 'Emma');
 INSERT INTO matieres (matiere) VALUES ('Français');
 INSERT INTO matieres (matiere) VALUES ('Math');
 INSERT INTO matieres (matiere) VALUES ('Histoire');
-INSERT INTO matieres (matiere) VALUES ('Anglais');*/
+INSERT INTO matieres (matiere) VALUES ('Anglais');
