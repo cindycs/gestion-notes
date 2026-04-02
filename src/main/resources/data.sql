0fc846cd89374ed041b58a44b078be18aa37e042
Cindy SCHAAL <schaal.cindy@gmail.com>
2026-04-02 15:55:44 +0200

    Nouvelle BDD qui prend en compte toute la structure de la BDD

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
-INSERT INTO notes (note, id_eleve, id_matiere) VALUES (12,1,2);
-INSERT INTO notes (note, id_eleve, id_matiere) VALUES (18, 2, 4);
-INSERT INTO notes (note, id_eleve, id_matiere) VALUES (20, 4, 2);
 
 INSERT INTO eleves (nom, prenom) VALUES ('Dupont', 'Jean');
 INSERT INTO eleves (nom, prenom) VALUES ('Martin', 'Sophie');
 INSERT INTO eleves (nom, prenom) VALUES ('Durand', 'Lucas');
 INSERT INTO eleves (nom, prenom) VALUES ('Bernard', 'Emma');
 
-
-INSERT INTO matieres (matiere) VALUES ('Français');
+INSERT INTO matieres (matiere) VALUES ('Francais');
 INSERT INTO matieres (matiere) VALUES ('Math');
 INSERT INTO matieres (matiere) VALUES ('Histoire');
 INSERT INTO matieres (matiere) VALUES ('Anglais');
+
+INSERT INTO notes (note, id_eleve, id_matiere) VALUES (12,1,2);
+INSERT INTO notes (note, id_eleve, id_matiere) VALUES (18,2,4);
+INSERT INTO notes (note, id_eleve, id_matiere) VALUES (20,4,2);
